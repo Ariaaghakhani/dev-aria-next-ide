301a30116296c23d3220035076a179dfd06aab9b
Aria Aghakhani <48532756+Ariaaghakhani@users.noreply.github.com>
2025-02-02 23:51:46 +0330

    1 Feb 2025

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,12 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
     // output: 'export',
+    // trailingSlash: true,
+    // basePath: "",
+    // assetPrefix:'./',
+    // images: {
+    //     unoptimized: true, // ✅ Ensures Next.js images work in static mode
+    // },
 };
 
 export default nextConfig;
--- src/app/layout.js
+++ src/app/layout.js
@@ -1,7 +1,8 @@
 import "./globals.css";
 import Header from "../components/Header";
 import Footer from "../components/Footer";
-import {Inter} from 'next/font/google';
+import {Inter} from "next/font/google";
+
 import {Providers} from "@/app/provider";
 
 const inter = Inter({
