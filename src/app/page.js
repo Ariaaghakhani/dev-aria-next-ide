8cc53e5532e6e2d4b5a014883e911ca3e4339e05
Aria Aghakhani <48532756+Ariaaghakhani@users.noreply.github.com>
2025-10-25 20:14:14 +0330

    Push test

--- src/app/page.js
+++ src/app/page.js
@@ -5,6 +5,7 @@ import Main from "@/components/Main";
 export default function Home() {
     return (
         <div className="bg-smokeWhite mx-auto">
+            HI
             <Main/>
         </div>
     );
